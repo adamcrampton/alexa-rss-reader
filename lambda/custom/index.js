5e7b48405088731cc2f81ad26b1c1301a9d41743
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-10-30 12:11:34 +1100

    Whups, forgot to re-comment node server stuff

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -103,17 +103,17 @@ exports.handler = function(event, context, callback) {
 
 
 // Uncomment to fire up local Node server
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Test Page\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Test Page\n');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
